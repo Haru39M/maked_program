eddf9fe1492c6456aad6e853c45d253986d2b51f
Haru39M <s2071149@s.konan-u.ac.jp>
2020-12-25 13:59:35 +0900

    add alert

--- project/WEBAPP/flask-tutorial/flaskr/static/metoo.js
+++ project/WEBAPP/flask-tutorial/flaskr/static/metoo.js
@@ -7,6 +7,7 @@ function post_metoo() {
       contentType: 'application/json',
       success: function (data) {
         window.alert("success!");
+        window.alert("success2");
         const metoo = JSON.parse(data.ResultSet).metoo//.metooのところは←jsonのキー.index.htmlに書いたid = metoo の部分にjsonデータから取り出して入れる。blog.pyでデータベースから取り出したデータをjsonにして、/metoo_postにアクセスされるとidにデータを入れる
         const test = "234"//試験用
         document.getElementById('metoo').innerHTML = metoo
